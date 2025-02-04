ac896741a13c26f9ed90bc5984af78695e78e662
George Miloshevich <miloshevich05@gmail.com>
2025-02-04 08:37:10 +0100

    parsing inputs to src/downscale.py

--- src/downscale.py
+++ src/downscale.py
@@ -2,20 +2,23 @@
 
 """
 This script processes HDF5 files by applying specified filters and saves the processed data as pickle files.
+
+Arguments:
+    --path (str): The base directory path for reading and writing files.
+    --read_folder (str): The folder name where input HDF5 files are located.
+    --write_folder (str): The folder name where output pickle files will be saved.
+
 Variables:
     filters (list): A list of dictionaries specifying the filters to apply.
-    path (str): The base directory path for reading and writing files.
-    read_folder (str): The folder name where input HDF5 files are located.
-    write_folder (str): The folder name where output pickle files will be saved.
 
 Processing:
     1. Iterate over each filename in filenames_list.
     2. Load the HDF5 file.
     3. Extract data for each field in the file.
     4. Apply specified filters to the data.
-    6. Save the processed data as a pickle file in the write_folder.
-    7. Copy the SimulationData.txt file from the read_folder to the write_folder.
-    8. Modify the 'Number of cells (x)' and 'Number of cells (y)' lines in the SimulationData.txt file.
+    5. Save the processed data as a pickle file in the write_folder.
+    6. Copy the SimulationData.txt file from the read_folder to the write_folder.
+    7. Modify the 'Number of cells (x)' and 'Number of cells (y)' lines in the SimulationData.txt file.
 
 Note:
     - The script assumes that the HDF5 files have a specific structure with data stored under "/Step#0/Block/".
@@ -29,21 +32,28 @@ import pickle
 import glob
 import os
 import shutil
+import argparse
 
 filters=[{'name': 'uniform_filter', 'size': 4, 'axes': (1,2), 'mode' : 'wrap'},
                 {'name': 'zoom', 'zoom': (1, 0.25, 0.25), 'mode' : 'grid-wrap'}]
-path = '/volume1/scratch/share_dir/peppe/'
-read_folder = 'data'
-write_folder = 'data_filter'
+parser = argparse.ArgumentParser(description='Process HDF5 files and apply filters.')
+parser.add_argument('--path', type=str, default='/volume1/scratch/share_dir/peppe/', help='The base directory path for reading and writing files.')
+parser.add_argument('--read_folder', default='data', type=str, required=True, help='The folder name where input HDF5 files are located.')
+parser.add_argument('--write_folder', default='data_filter', type=str, required=True, help='The folder name where output pickle files will be saved.')
+args = parser.parse_args()
+
+path = args.path
+read_folder = args.read_folder
+write_folder = args.write_folder
 # Get all filenames in the read_folder
 all_filenames = glob.glob(f'{path}{read_folder}/*.h5')
 filenames_list = [os.path.basename(f) for f in all_filenames]
 
 for filename in filenames_list:
-    print(f"Processing {read_filename}")
-    print(f"Writing to {write_filename}")
     read_filename = f'{path}{read_folder}/{filename}'
     write_filename = f'{path}{write_folder}/{filename}.pkl'
+    print(f"Processing {read_filename}")
+    print(f"Writing to {write_filename}")
     # Load the file
     verbose=False
     data = {}
